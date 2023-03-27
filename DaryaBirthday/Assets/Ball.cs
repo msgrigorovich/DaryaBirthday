eb3f2fa7b9c6a11eaaa25134a850adfc3a5ff7cc
Diana Grigorovich <ms.grigorovich@yandex.ru>
2023-03-28 01:37:55 +0300

    added ball

---
+++ DaryaBirthday/Assets/Ball.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    public float maxBallSpeed = 50f;
+
+    
+
+    public float force = 1000f;
+    public GameObject ballPrediction;
+
+    private Vector2 startPostition;
+    private Vector2 endPosition;
+
+    private Rigidbody physics;
+
+    void Awake()
+    {
+        physics = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+        physics.isKinematic = true;
+    }
+    void Update()
+    {
+        physics.velocity = Vector3.ClampMagnitude(physics.velocity, maxBallSpeed);
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPostition = GetMousePosition();
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 dragPosition = GetMousePosition();
+            Vector2 power = startPostition - dragPosition;
+
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            endPosition = GetMousePosition();
+
+            Vector2 power = startPostition - endPosition;
+            physics.isKinematic = false;
+
+            physics.AddForce(power * force, ForceMode.Force);
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    private Vector2 GetMousePosition()
+    {
+        return Camera.main.ScreenToViewportPoint(Input.mousePosition);
+    }
+    
+}
